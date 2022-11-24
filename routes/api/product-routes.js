0388b81efe84edd6d1e2637b15c732cea0bf1e8c
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2022-11-23 18:08:10 -0700

    api '/' routes established

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,16 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+  try {
+    const productData = await Product.findAll();
+    res.status(200).json(productData).join(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
+ 
 });
 
 // get one product
