3660f47f58bbdd630d93c0a8429206428e57cade
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2022-11-29 08:53:04 -0700

    update category successfull

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
+ 
+// find all categories
 router.get('/', async (req, res) => {
-  // find all categories
   try {
     const categoryData = await Category.findAll({
     // be sure to include its associated Products
@@ -22,8 +22,8 @@ router.get('/', async (req, res) => {
     } 
 });
 
+// find one category by its `id` value
 router.get('/:id', async (req, res) => {
-  // find one category by its `id` value
   try {
     const categoryData = await Category.findByPk(req.params.id, {
       // be sure to include its associated Products
@@ -40,11 +40,9 @@ router.get('/:id', async (req, res) => {
       res.status(500).json(err);
       } 
   });
-  
 
-
+// create a new category
 router.post('/', async (req, res) => {
-  // create a new category
   try {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
@@ -53,12 +51,14 @@ router.post('/', async (req, res) => {
   }
 });
 
+// update a category by its `id` value
 router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
   try{
-
     const categoryData = await Category.update
-  ({
+  (
+    {category_name: req.body.category_name,
+    },
+    {
     where: {
       id: req.params.id
     }
